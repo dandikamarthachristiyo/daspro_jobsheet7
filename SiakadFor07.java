a06cacaacdf033c883acfb8e2e8f4bd17c365b89
dandikamarthachristiyo <dandika.martha@gmail.com>
2024-10-10 18:39:13 +0700

    pertanyaan percobaan 1

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -4,11 +4,17 @@ public class SiakadFor07 {
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100; 
+        int lulus = 0, tidakLulus= 0;
          
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            if (nilai >= 60 ) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
             if (nilai > tertinggi) {
                 tertinggi = nilai;
                 
@@ -19,6 +25,8 @@ public class SiakadFor07 {
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Banyak Mahasiswa yang lulus: " + lulus);
+        System.out.println("Banyak Mahasiswa yang tidak lulus: " + tidakLulus);
         
         sc.close();
     }
